d6c65473482a57aa41741f33747dbccbe1ca5d4f
sea <90987597+seawachi@users.noreply.github.com>
2026-02-16 18:39:28 +0700

    add PowerPump game configuration to the GameRegistry

--- games/registry.ts
+++ games/registry.ts
@@ -14,6 +14,7 @@ import { TaxiDriverGameConfig } from './game-15-taxidriver/config';
 import { MinerGameConfig } from './game-10-miner/config';
 import { WordRecognizeGameConfig } from './game-14-wordrecognize/config';
 import { DoorGuardianGameConfig } from './game-16-doorguardian/config';
+import { PowerPumpGameConfig } from './game-11-power-pump/config';
 
 export const GameRegistry: Record<string, Phaser.Types.Core.GameConfig> = {
   'game-00-example': ExampleGameConfig,
@@ -27,6 +28,7 @@ export const GameRegistry: Record<string, Phaser.Types.Core.GameConfig> = {
   'game-07-pinkcup': PinkCupGameConfig,
   'game-09-tube-sort': TubeSortGameConfig,
   'game-10-miner': MinerGameConfig,
+  'game-11-power-pump': PowerPumpGameConfig,
   'game-12-gridhunter': GridHunterGameConfig,
   'game-13-boxpattern': BoxPatternGameConfig,
   'game-15-taxidriver': TaxiDriverGameConfig,
